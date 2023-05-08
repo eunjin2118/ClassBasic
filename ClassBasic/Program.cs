989687a3f299d5948cfbdbb1a77b5570f8e0265f
Eunjin Pyeon <s2131@e-mirim.hs.kr>
2023-05-08 12:17:30 +0900

    클래스 이름이 겹치는 경우

--- ClassBasic/Program.cs
+++ ClassBasic/Program.cs
@@ -10,6 +10,18 @@ namespace MyApp // Note: actual namespace depends on the project name.
             ABCClass abc2; // 동급의 클래스
             // 중첩 클래스
             InnerClass inn; // 이 클래스 안에서 잠깐 사용하는 클래스
+
+            // 이름 충돌이 날 경우 가장 가까운것을 가져 옴
+            Math math = new Math();
+            //Console.WriteLine(Math.Abs(-10));
+        }
+    }
+
+    class Math
+    {
+        static public int Abs()
+        {
+            return 0;
         }
     }
     class InnerClass
