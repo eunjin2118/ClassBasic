e1de9ab7dca942252c9a4dc92df1f01f198d69f4
Eunjin Pyeon <s2131@e-mirim.hs.kr>
2023-05-08 12:14:34 +0900

    Class

---
+++ ClassBasic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ThirdClass abc; // 여기에서 먼저 만들고 단축키로 클래스 생성 가능
+            ABCClass abc2; // 동급의 클래스
+            // 중첩 클래스
+            InnerClass inn; // 이 클래스 안에서 잠깐 사용하는 클래스
+        }
+    }
+    class InnerClass
+    {
+    }
+
+}
+
+internal class ABCClass
+{
+}
---
+++ ClassBasic/ThirdClass.cs
@@ -0,0 +1,6 @@
+﻿namespace MyApp
+{
+    internal class ThirdClass
+    {
+    }
+}
